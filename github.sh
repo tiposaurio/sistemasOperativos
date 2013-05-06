3a59260ae53d524fdbf893bc1ddbed4e527eb056
tiposaurio <tiposurio@gmail.com>
2013-05-06 11:38:27 -0500

    test nro uno

---
+++ github.sh
@@ -0,0 +1,14 @@
+git init 
+git config --global user.name "tiposaurio"
+git cinfig --global user.name tiposaurio@gmai.com
+git remote add origin git@github.com:tiposaurio/examenSO.git
+git add .
+git status
+git commit -a -m "mis archivos.."
+git status
+git pull origin master
+git push origin master
+git status
+ls
+
+echo "tarea de github terminada..."
